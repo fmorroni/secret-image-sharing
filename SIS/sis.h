8df6ec91a99607915fb1b97faa0a7abf515b84ab
Franco Morroni <fmorroni99@gmail.com>
2025-05-14 17:08:12 -0300

    chore: arguments rename

--- SIS/sis.h
+++ SIS/sis.h
@@ -4,7 +4,7 @@
 #include "../bmp/bmp.h"
 #include <sys/types.h>
 
-void sisShadows(BMP bmp, u_char r, u_char n);
-BMP sisRecover(u_char r, BMP shadows[r]);
+void sisShadows(BMP bmp, u_char min_shadows, u_char tot_shadows);
+BMP sisRecover(u_char min_shadows, BMP shadows[min_shadows]);
 
 #endif
