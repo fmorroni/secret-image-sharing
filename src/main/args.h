b2d13f9f6a0e5243855db95bcc62618bafaba5fe
Franco Morroni <fmorroni99@gmail.com>
2025-05-17 21:25:53 -0300

    fix: u_int8_t to uint8_t
    Stupid sys/types.h

--- src/main/args.h
+++ src/main/args.h
@@ -3,17 +3,17 @@
 
 #include "../bmp/bmp.h"
 #include <stdbool.h>
-#include <sys/types.h>
+#include <stdint.h>
 
 typedef struct Args {
   bool distribute;
   bool recover;
   const char* secret_filename;
-  u_int8_t min_shadows;
-  u_int8_t tot_shadows;
+  uint8_t min_shadows;
+  uint8_t tot_shadows;
   const char* directory;
   char* _directory_allocated;
-  u_int8_t parsed_bmps;
+  uint8_t parsed_bmps;
   BMP* dir_bmps;
 } Args;
 
